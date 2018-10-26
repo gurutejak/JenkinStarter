717f72c0942b57102fea5553a2972fe3c03d961d
Guru Ketepalle <guru.ketepalle@cigna.com>
2018-10-26 13:01:25 +0530

    Initial Commit

---
+++ JenkinsStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkinsStarter
+//
+//  Created by Teja Ketepalle on 26/10/18.
+//  Copyright © 2018 Teja Ketepalle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
